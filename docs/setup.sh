8ad1c39d9915b913227e2faecfd97d1422a9c204
Steve Roach <sroachst@gmail.com>
2026-01-30 20:40:31 -0500

    Configure `application-local.yml` and update `application.yml` paths
    - Added `application-local.yml` with localized configuration including server, management, app, and logging settings.
    - Updated directory paths in `application.yml` for GitHub staging and web, as well as allowed base paths for security.

---
+++ docs/setup.sh
@@ -0,0 +1,6 @@
+GITHUB_STAGING_DIR="/trvapps/docops/staging/docs"
+GITHUB_WEB_DIR="/trvapps/docops/www/docs"
+GITHUB_DISABLE_SSL=false
+ENCRYPTION_KEY=change me
+
+java -jar ../target/converter-2026.0.0.jar
